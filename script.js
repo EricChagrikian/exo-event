65bfaa49fd3f7f9e30e3a76cabaa4214e4444f5d
Eric Chagrikian <echagrikian@hotmail.com>
2022-04-26 14:01:46 +0200

    last

--- script.js
+++ script.js
@@ -19,28 +19,23 @@ const clickOnSquare = (e) => {
     let li = document.createElement('li')
     let ul = document.querySelector('ul')
     ul.appendChild(li)
-    li.innerText = getElapsedTime() + ' Created a new green square'
+    li.innerText = '[' + getElapsedTime() + ']' + ' Created a new green square'
   }
   if(e.target.classList[1] === "violet") {
     div.className = "displayedsquare violet"
     let li = document.createElement('li')
     let ul = document.querySelector('ul')
     ul.appendChild(li)
-    li.innerText = getElapsedTime() + ' Created a new violet square'
+    li.innerText = '[' + getElapsedTime() + ']' + ' Created a new violet square'
   }
   if(e.target.classList[1] === "orange") {
     div.className = "displayedsquare orange"
     let li = document.createElement('li')
     let ul = document.querySelector('ul')
     ul.appendChild(li)
-    li.innerText = getElapsedTime() + ' Created a new orange square'
+    li.innerText = '[' + getElapsedTime() + ']' + ' Created a new orange square'
   }
-
-
 }
-
-
-
 
 
 const actionSquares = document.querySelectorAll('.actionsquare')
@@ -52,10 +47,12 @@ const getRandomNumber = (maxNum) => {
   return Math.floor(Math.random() * maxNum);
 }
 
+
+  
 document.body.onkeyup = function(e) {
   if (e.key == " " ||
       e.code == "Space" ||        
-      e.key == 32        
+      e.key == "Space"        
   ) {
     function randbg() {
       let r = getRandomNumber(255);
@@ -66,21 +63,23 @@ document.body.onkeyup = function(e) {
       let li = document.createElement('li')
       let ul = document.querySelector('ul')
       ul.appendChild(li)
-      li.innerText = getElapsedTime() + ' Changed background color'
+      li.innerText = '[' + getElapsedTime() + ']' + ' Changed background color'
       return randomcolors
   }
   document.body.style.backgroundColor = randbg(); 
   }
-}
 
-
-document.body.onkeyup = function(e) {
-  if (e.key == " " ||
-      e.code == "KeyL" ||        
-      e.key == "l"        
+  if (e.code == "KeyL" ||        
+      e.key == 'l'
   ) {
-    let ul = document.createElement('ul')
-    ul.innerHTML = ''
-    console.log("key press")
-  }
+      let ul = document.querySelector('ul')
+      ul.innerHTML = '' 
+    } 
+      if (e.code == "KeyS" ||
+      e.key == 's'
+ ) {
+      let main = document.querySelector('main')
+      let section = main.children[0]
+      section.innerHTML = ''
+ }
 }
